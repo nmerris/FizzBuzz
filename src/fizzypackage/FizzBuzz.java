fea59a35e0af5ea904294f5ca51a157e0a12d90c
nmerris <nate.merris@gmail.com>
2017-07-27 08:30:13 -0400

    Remove introductory flavor text

--- src/fizzypackage/FizzBuzz.java
+++ src/fizzypackage/FizzBuzz.java
@@ -13,7 +13,7 @@ public class FizzBuzz {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Prepare for a FizzBuzz bonanza!");
+//		System.out.println("Prepare for a FizzBuzz bonanza!");
 		for(int i = 1; i <= 100; i++) {
 			if(i % 3 == 0 && i % 5 == 0) { // multiple of 3 and 5
 				System.out.println("FizzBuzz");
